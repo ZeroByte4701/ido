4ca588ef18487d8542b31a911ddbb27feed8858c
ZeroByte4701 <94198465+ZeroByte4701@users.noreply.github.com>
2024-01-31 06:28:14 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -4,10 +4,10 @@ import { GambaStandardTokens, TokenMeta } from 'gamba-react-ui-v2'
 export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://api.mainnet-beta.solana.com"
 
 // Change this value to your Solana address
-export const PLATFORM_CREATOR_ADDRESS = 'V2grJiwjs25iJYqumbHyKo5MTK7SFqZSdmoRaj8QWb9'
+export const PLATFORM_CREATOR_ADDRESS = '3WdcfH6Gwe5bT6sXueUu7gb7U7dMq1yUFm351RJ4zLQT'
 
 // Appears in ShareModal
-export const PLATFORM_SHARABLE_URL = 'v2-play.gamba.so'
+export const PLATFORM_SHARABLE_URL = 'topia.casino'
 
 // List of tokens supported by this platform
 export const TOKENS: TokenMeta[] = [
