81ea771d47ebff13b9aa74414ef4d2eaaede096e
ZeroByte4701 <94198465+ZeroByte4701@users.noreply.github.com>
2024-02-01 01:37:13 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -36,7 +36,7 @@ function Root() {
                 creator={PLATFORM_CREATOR_ADDRESS}
                 games={GAMES}
                 tokens={TOKENS}
-                defaultCreatorFee={2}
+                defaultCreatorFee={0.02}
                 defaultJackpotFee={0.001}
               >
                 <App />
